8be6b0d776ad3b0eda755ce60462b2ffc5d59a14
YaroslavShinkarenko <shinkarenkoyaroslav@gmail.com>
2022-05-21 17:47:15 +0300

    добавлен файл скриптов

--- scripts/script.js
+++ scripts/script.js
@@ -6,61 +6,54 @@ let profileAbout = profileContainer.querySelector(".profile__description");
 let profileAddButton = profileContainer.querySelector(".profile__add-button");
 
 let profileForm = document.querySelector(".popup");
-let nameInput = profileForm.querySelector(".popup__input_profile_name");
-let aboutInput = profileForm.querySelector(".popup__input_profile_about");
 let closeFormButton = profileForm.querySelector(".popup__close-button");
+let saveFormButton = profileForm.querySelector(".popup__save-button");
+let places = content.querySelector(".places");
+let inputs = document.querySelectorAll("input");
 
-let places = content.querySelectorAll(".place");
-let likeButton = places.querySelector("ul li .place__like-button");
+let likeButtons = document.querySelectorAll(".place__like-button");
 
-// nameInput.textContent = "${profilleName}";
-// nameInput.textContent = "${profilleAbout}";
+for (let likeButton of likeButtons) {
+  likeButton.addEventListener("click", setLike);
+}
+
+function setLike(event) {
+  let likeButton = this;
+  if (likeButton.classList.contains("place__like-button_active")) {
+    likeButton.classList.remove("place__like-button_active");
+  } else {
+    likeButton.classList.add("place__like-button_active");
+  }
+}
 
 function openProfileForm() {
-  profileForm.setAttribute("active", true);
   profileForm.classList.add("popup_active");
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
 }
 
 profileEditButton.addEventListener("click", openProfileForm);
 
 function closeProfileForm() {
-  profileForm.setAttribute("disable", true);
   profileForm.classList.remove("popup_active");
 }
 
 closeFormButton.addEventListener("click", closeProfileForm);
 
-function setLike() {
-  likeButton.classList.add("place__like-button_active");
-}
+let editForm = document.querySelector("[name=profileEditForm]");
+let nameInput = document.querySelector(".popup__input_profile_name");
+let aboutInput = document.querySelector(".popup__input_profile_about");
 
-likeButton.addEventListener("click", setLike);
-
-function saveButton() {
-  songsContainer.innerHTML += `
-      <div class="song">
-        <h4 class="song__artist">Linkin Park</h4>
-        <p class="song__title">Numb</p>
-        <button class="song__like"></button>
-      </div>
-`;
-}
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
+  evt.preventDefault();
 
-  // Получите значение полей jobInput и nameInput из свойства value
-  paragraph.textContent = nameInput.value;
-  paragraph.textContent = aboutInput.value;
-  // Выберите элементы, куда должны быть вставлены значения полей
+  let newName = nameInput.value;
+  let newAbout = aboutInput.value;
 
-  // Вставьте новые значения с помощью textContent
+  profileName.textContent = newName;
+  profileAbout.textContent = newAbout;
+
+  closeProfileForm();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener("submit", formSubmitHandler);
+editForm.addEventListener("submit", formSubmitHandler);
