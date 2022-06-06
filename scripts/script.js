fe4f46d0273cd0626df99e1844b7bed5a6fbefd1
YaroslavShinkarenko <shinkarenkoyaroslav@gmail.com>
2022-06-06 19:18:10 +0300

    initial commit

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,16 @@ let profileName = document.querySelector(".profile__name");
 let profileAbout = document.querySelector(".profile__description");
 let profileForm = document.querySelector(".popup_profile");
 let placeAddForm = document.querySelector(".popup_place");
+let placeInspector = document.querySelector(".popup_place-inspector");
+let popup = document.querySelector(".popup");
+
+let editForm = document.querySelector(".popup__profile-edit-form");
+let nameInput = document.querySelector(".popup__input_profile_name");
+let aboutInput = document.querySelector(".popup__input_profile_about");
+
+let addForm = document.querySelector(".popup__place-add-form");
+let placeNameInput = document.querySelector(".popup__input_place-name");
+let placeLinkInput = document.querySelector(".popup__input_place_image-link");
 
 let closeProfileFormButton = document.querySelector(
   ".popup__close-profile-form-button"
@@ -9,31 +19,44 @@ let closeProfileFormButton = document.querySelector(
 let closePlaceAddButton = document.querySelector(
   ".popup__close-place-add-form-button"
 );
+let closePlaceInspectorButton = document.querySelector(
+  ".popup__close-place-inspector-button"
+);
 let likeButtons = document.querySelectorAll(".place__like-button");
 let placeAddButton = document.querySelector(".profile__place-add-button");
 let profileEditButton = document.querySelector(".profile__edit-button");
+let popupCloseButton = document.querySelector(".popup__close-button");
 
-////// PROFILE FORM /////
+const placesListItem = document.querySelector(".places__list");
+const placeTemplate = document.querySelector(".place-template").content;
+
+const createPlaceAddButton = document.querySelector(
+  ".popup__create-place-button"
+);
+
+////// PROFILE FORM OPENER /////
+
+function popupToggle(popup) {
+  if (popup.classList.contains("popup_active")) {
+    popup.classList.remove("popup_active");
+  } else {
+    popup.classList.add("popup_active");
+  }
+}
+
+function popupClose() {}
 
 function popProfileForm() {
   nameInput.value = profileName.textContent;
   aboutInput.value = profileAbout.textContent;
 
-  if (profileForm.classList.contains("popup_active")) {
-    profileForm.classList.remove("popup_active");
-  } else {
-    profileForm.classList.add("popup_active");
-  }
+  popupToggle(profileForm);
 }
 
 closeProfileFormButton.addEventListener("click", popProfileForm);
 profileEditButton.addEventListener("click", popProfileForm);
 
 ////// PROFILE FORM SUBMIT /////
-
-let editForm = document.querySelector(".popup__profile-edit-form");
-let nameInput = document.querySelector(".popup__input_profile_name");
-let aboutInput = document.querySelector(".popup__input_profile_about");
 
 function profileFormSubmitHandler(evt) {
   evt.preventDefault();
@@ -49,7 +72,12 @@ function profileFormSubmitHandler(evt) {
 
 editForm.addEventListener("submit", profileFormSubmitHandler);
 
+////// 1 /////
 ////// PLACES ARRAY /////
+
+/// Отрендерить карточки - динамически добавить на страницу каждую карточку из массива places
+/// добавить на страницу каждую карточку - создать копию темплейт элемента, заполнить его содержимым эллемента массива places, навесить обработчики события для лайка, удаления и просмотра карточки
+/// выбрать эллемент через ДОМ, повесить на него обработчик события и действие котороое нужно совершить при определенном событии на странице
 
 const places = [
   {
@@ -66,7 +94,7 @@ const places = [
   },
   {
     name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    link: "https://images.unsplash.com/photo-1580993123109-63aea48b2807?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2418&q=80",
   },
   {
     name: "Холмогорский район",
@@ -74,54 +102,91 @@ const places = [
   },
   {
     name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    link: "https://images.unsplash.com/photo-1617835594990-7cd5a9b5d153?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
   },
 ];
 
-////// PLACES RENDER /////
+////// 2 /////
+////// PLACE ADD FORM OPENER /////
 
-const placesListItem = document.querySelector(".places__list");
-const placeTemplate = document.querySelector(".place-template").content;
-
-places.forEach(function (element) {
-  const placeItem = placeTemplate.cloneNode(true);
-
-  placeItem.querySelector(".place__photo").src = element.link;
-  placeItem.querySelector(".place__name").textContent = element.name;
-  placesListItem.append(placeItem);
-});
-
-////// SET LIKE /////
-
-// for (let likeButton of likeButtons) {
-//   likeButton.addEventListener("click", setLike);
-// }
-
-// function setLike() {
-//   let likeButton = this;
-//   if (likeButton.classList.contains("place__like-button_active")) {
-//     likeButton.classList.remove("place__like-button_active");
-//   } else {
-//     likeButton.classList.add("place__like-button_active");
-//   }
-// }
-
-////// POP PLACE ADD FORM /////
+/*
+ * @description делаем попап видимым на странице и скрываем его при сабмите или закрытиии
+ */
 
 function popPlaceAddForm() {
-  if (placeAddForm.classList.contains("popup_active")) {
-    placeAddForm.classList.remove("popup_active");
-  } else {
-    placeAddForm.classList.add("popup_active");
-  }
+  popupToggle(placeAddForm);
 }
 
 closePlaceAddButton.addEventListener("click", popPlaceAddForm);
 placeAddButton.addEventListener("click", popPlaceAddForm);
 
+/*
+ * @description по кнопке сабмита "создать" добавляется карточка с введенными парамметрами name и link
+ */
+
+function addPlaceOnSubmit() {
+  createPlaceAddButton.addEventListener("click", function () {
+    const name = document.querySelector(".popup__input_place-name");
+    const link = document.querySelector(".popup__input_place_image-link");
+
+    renderPlace(name.value, link.value);
+
+    name.value = "";
+    link.value = "";
+  });
+}
+
+addPlaceOnSubmit();
+
+////// PLACE ADD FORM SUBMIT /////
+
+/*
+ * @description отменяет стандартную обработку формы и закрывает окно формы
+ */
+
+function placeAddFormSubmitHandler(evt) {
+  evt.preventDefault();
+
+  popPlaceAddForm();
+}
+
+addForm.addEventListener("submit", placeAddFormSubmitHandler);
+
+////// 3, 4, 5 /////
+////// PLACES RENDER /////
+
+/*
+ * @description функция удаления ближайшего ДОМ элемента с классом place
+ */
+
+function removePlaceItem(event) {
+  let buttonElement = event.target;
+  let placeItemElement = buttonElement.closest(".place");
+  placeItemElement.remove();
+}
+
+/*
+ * @description для каждого элемента массива renderingPlaces происходит добавление на страницу
+ * @arg renderingPlaces - это массив элементы которого будут отрендерены
+ */
+
+function cardsRender(renderingPlaces) {
+  renderingPlaces.forEach(function (element) {
+    renderPlace(element.name, element.link);
+  });
+}
+
+cardsRender(places);
+
 ////// PLACE ADD /////
 
-function addPlace(placeNameValue, placeLinkValue) {
+/*
+ * @description берем эллемент из массива и наполняем клон темплейт элемента его содержимым, на кнопки назначаются обработчики событий, а затем карточка добавляется в начало массива
+ * @arg1 placeNameValue - имя карточки
+ * @arg2 placeLinkValue - ссылка на фото
+ */
+
+function renderPlace(placeNameValue, placeLinkValue) {
   placeItem = placeTemplate.cloneNode(true);
 
   placeItem.querySelector(".place__photo").src = placeLinkValue;
@@ -133,39 +198,36 @@ function addPlace(placeNameValue, placeLinkValue) {
       evt.target.classList.toggle("place__like-button_active");
     });
 
-  placesListItem.append(placeItem);
+  let removeButtonElement = placeItem.querySelector(".place__delete-button");
+  removeButtonElement.addEventListener("click", removePlaceItem);
+
+  placeItem
+    .querySelector(".place__photo")
+    .addEventListener("click", function () {
+      placeInspectorOpen(placeNameValue, placeLinkValue);
+    });
+
+  placesListItem.prepend(placeItem);
 }
 
-const createPlaceAddButton = document.querySelector(
-  ".popup__create-place-button"
-);
+////// 6 /////
+////// PLACE INSPECTOR /////
 
-createPlaceAddButton.addEventListener("click", function () {
-  const name = document.querySelector(".popup__input_place-name");
-  const link = document.querySelector(".popup__input_place_image-link");
+/*
+ * @description открывает попап с картинкой и названием из карточки
+ */
 
-  addPlace(name.value, link.value);
+function placeInspectorOpen(name, link) {
+  let placeImageFull = name;
+  let placeAbout = link;
 
-  name.value = "";
-  link.value = "";
-});
+  const placeInspectorImage = document.querySelector(".place-inspector__image");
+  const placeInspectorName = document.querySelector(".place-inspector__name");
 
-////// PLACE ADD FORM SUBMIT /////
+  placeInspectorImage.src = link;
+  placeInspectorName.textContent = name;
 
-let addForm = document.querySelector(".popup__place-add-form");
-let placeNameInput = document.querySelector(".popup__input_place-name");
-let placeLinkInput = document.querySelector(".popup__input_place_image-link");
-
-function placeAddFormSubmitHandler(evt) {
-  evt.preventDefault();
-
-  let newPlaceName;
-  let newPlaceLink;
-
-  placeNameValue = newPlaceName;
-  placeLinkValue = newPlaceLink;
-
-  popPlaceAddForm();
+  popupToggle(placeInspector);
 }
 
-addForm.addEventListener("submit", placeAddFormSubmitHandler);
+closePlaceInspectorButton.addEventListener("click", placeInspectorOpen);
